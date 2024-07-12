c529da45ce381c10bee14fff012d61062cad5900
Aman_Singh <100147386+AmanSinghX@users.noreply.github.com>
2024-07-12 13:15:19 +0530

    Add files via upload

---
+++ 01basicreact/src/AddOn.js
@@ -0,0 +1,7 @@
+function AddOn() {
+    return (
+     <h2>Add-on for CRA app</h2>   
+    )
+}
+
+export default AddOn
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import AddOn from "./AddOn.js";
+
+function App() {
+  return (
+    <>
+    <h1> Hello Everyone! || Myself Aman Singh, this is introduction to React.</h1>
+    <AddOn/>
+    </>
+    
+  );
+}
+
+export default App;
